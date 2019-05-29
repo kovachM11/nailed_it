fb56e8f1be75ece9de198b08d34d25ffbab45b5e
Raghu Betina <raghu@firstdraft.com>
2019-05-29 22:37:22 +0000

    Whitelist columns in Development for ActiveAdmin

--- app/admin/developments.rb
+++ app/admin/developments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Development do
+
+ permit_params :user_id, :trigger, :action_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
