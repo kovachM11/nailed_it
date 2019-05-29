fae9548bdf7d854c73345286afb78ea3bc7b0b31
Raghu Betina <raghu@firstdraft.com>
2019-05-29 22:37:51 +0000

    Only progress_photo user can modify progress_photo

--- app/controllers/progress_photos_controller.rb
+++ app/controllers/progress_photos_controller.rb
@@ -1,4 +1,14 @@
 class ProgressPhotosController < ApplicationController
+  before_action :current_user_must_be_progress_photo_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_progress_photo_user
+    progress_photo = ProgressPhoto.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == progress_photo.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @progress_photos = ProgressPhoto.all
 
